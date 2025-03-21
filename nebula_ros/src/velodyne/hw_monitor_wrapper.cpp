5f12df8504d47f9d084b6973e443ec92db179b64
Mukunda Bharatheesha <mukunda.bharatheesha@nobleo.nl>
2025-03-21 10:50:25 +0100

    Restore pps and voltage diagnostics.
    Signed-off-by: Mukunda Bharatheesha <mukunda.bharatheesha@nobleo.nl>

--- nebula_ros/src/velodyne/hw_monitor_wrapper.cpp
+++ nebula_ros/src/velodyne/hw_monitor_wrapper.cpp
@@ -163,12 +163,12 @@ void VelodyneHwMonitorWrapper::initialize_velodyne_diagnostics()
 
   diagnostics_updater_.add(
     "velodyne_status", this, &VelodyneHwMonitorWrapper::velodyne_check_status);
-  // diagnostics_updater_.add("velodyne_pps", this, &VelodyneHwMonitorWrapper::velodyne_check_pps);
+  diagnostics_updater_.add("velodyne_pps", this, &VelodyneHwMonitorWrapper::velodyne_check_pps);
   diagnostics_updater_.add(
     "velodyne_temperature", this, &VelodyneHwMonitorWrapper::velodyne_check_temperature);
   diagnostics_updater_.add("velodyne_rpm", this, &VelodyneHwMonitorWrapper::velodyne_check_rpm);
-  // diagnostics_updater_.add(
-  //   "velodyne_voltage", this, &VelodyneHwMonitorWrapper::velodyne_check_voltage);
+  diagnostics_updater_.add(
+    "velodyne_voltage", this, &VelodyneHwMonitorWrapper::velodyne_check_voltage);
 
   diagnostics_updater_.setPeriod(1.0);
 }
